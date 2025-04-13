69e00cca91432dc05d59eb56c9cf8e1aaf41ae5a
globalandlocal <itsabigbum@gmail.com>
2025-04-13 19:01:36 +0300

    7

--- async streamlit.py
+++ async streamlit.py
@@ -9,15 +9,17 @@ giga = GigaChat(
     credentials=api_key,
     verify_ssl_certs=False)
 st.title("💬 Чат с Gigachat")
-
+# создание истории сообщений
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# создание чата
 if prompt := st.chat_input("Напишите сообщение..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    #публикация вопроса от пользователя
     with st.chat_message("user"):
         st.markdown(prompt)
-
+    #публикация ответ от нейросети
     with st.chat_message("assistant"):
         response = giga.invoke(prompt).content
         st.markdown(response)
