57ffee0ba8f8a7a66c5acf903843ae0348847c1b
globalandlocal <itsabigbum@gmail.com>
2025-04-13 18:22:24 +0300

    2

--- async streamlit.py
+++ async streamlit.py
@@ -1,6 +1,36 @@
-import streamlit
+import dotenv
 import streamlit as st
+from openai import OpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_gigachat.chat_models import GigaChat
 
-streamlit.write('Здарова бандиты,че каво?)')
+api_key = dotenv.get_key('./keys.env','deepseek_key')
 
-x = streamlit.chat_input(placeholder='Пиши сюда и жди ответ')
+# Установите ваш API-ключ OpenAI (или другой совместимый API)
+client = OpenAI(api_key="YzJjODRlZjgtODYzNy00OWEyLTk5MGMtNThhZTlkOTkwZTI3OjljZTkwMWYzLTU1YWQtNDI0YS1iNWMyLTA0NTI4MWIwNTRiNg==",
+                base_url="https://gigachat.devices.sberbank.ru/api/v1")
+giga = GigaChat(
+    # Для авторизации запросов используйте ключ, полученный в проекте GigaChat API
+    credentials="YzJjODRlZjgtODYzNy00OWEyLTk5MGMtNThhZTlkOTkwZTI3OjljZTkwMWYzLTU1YWQtNDI0YS1iNWMyLTA0NTI4MWIwNTRiNg==",
+    verify_ssl_certs=False)
+st.title("💬 Чат с Gigachat")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if prompt := st.chat_input("Напишите сообщение..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = giga.invoke(prompt).content
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+
